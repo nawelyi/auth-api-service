6facd0ef9f1fd3d61ef011156a2f65158d103086
Nawel <naweltv@gmail.com>
2025-10-30 02:02:35 -0400

    feat(auth): implement /register endpoint with hashing and db

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes'
 
 dotenv.config();
 
@@ -8,9 +9,9 @@ const app = express();
 app.use(express.json());
 
 app.get('/api/health', (req: Request, res: Response) => {
-    res.status(200).json({ status: 'UP', message: 'Servidor base saludable' });
-})
+  res.status(200).json({ status: 'UP', message: 'Servidor base saludable' });
+});
 
-
+app.use('/api/auth', authRoutes);
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import app from './app';
 
-
 const PORT = process.env.PORT || 3000;
 
-
-
 app.listen(PORT, () => {
-    console.log(`Servidor base corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor base corriendo en http://localhost:${PORT}`);
 });
